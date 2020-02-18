725bc33adb3a94af6ecaaf25083b7938a04c60a1
Juan Velez Ballesteros <juanvelezballesteros@gmail.com>
2020-02-18 13:09:56 -0600

    Update terraform_backend.tf

--- terraform_backend.tf
+++ terraform_backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "bb55b6f311cd7376e9135897cf3a1dd5c9840bc7"
+    bucket = "arn:aws:s3:::jvelezb-terraform-backend-bucket"
     key    = "terraform_state_files/example_app.tfstate"
-    region = "eu-west-1"
-    encrypt = true
+    region = "us-east-1"
+    encrypt = false
   }
 }
